27b887cc08a2884e681c5dec9fb89119a3a9a21a
apines <aviad.pines@intel.com>
2016-11-12 19:14:09 +0200

    adding timeout

--- library/cdh_parcels.py
+++ library/cdh_parcels.py
@@ -4,55 +4,64 @@ def cm_api(cm_host, cm_api_version, cm_user, cm_pass):
     return ApiResource(cm_host, version=cm_api_version, username=cm_user, password=cm_pass)
 
 
-def download_parcel(cluster, parcel):
+def download_parcel(cluster, parcel, timeout):
     try:
         pd = parcel.start_download()
         if pd.success != True:
             result = "FAILED: Could not start downloading parcel"
-        while parcel.stage != 'DOWNLOADED':
+        curr_timeout = 0
+        while parcel.stage != 'DOWNLOADED' and curr_timeout < timeout:
             parcel = cluster.get_parcel(parcel.product, parcel.version)
             if parcel.state.errors:
                 return "FAILED: " + str(parcel.state.errors) + '.'
             time.sleep(15)
+            curr_timeout += 15
         if parcel.stage == 'DOWNLOADED':
-            return "OK" 
+            return "OK"
+        return "FAILED: Timed out"
     except Exception as e:
         return "FAILED: %s" % (e)   
 
 
-def distribute_parcel(cluster, parcel):
+def distribute_parcel(cluster, parcel, timeout):
     try:
         pd = parcel.start_distribution()
         if pd.success != True:
             result = "FAILED: Could not start distributing parcel"
-        while parcel.stage != 'DISTRIBUTED':
+        curr_timeout = 0
+        while parcel.stage != 'DISTRIBUTED' and curr_timeout < timeout:
             parcel = cluster.get_parcel(parcel.product, parcel.version)
             if parcel.state.errors:
                 return "FAILED: " + str(parcel.state.errors) + '.'
             time.sleep(15)
+            curr_timeout += 15
         if parcel.stage == 'DISTRIBUTED':
-            return "OK" 
+            return "OK"
+        return "FAILED: Timed out"
     except Exception as e:
-        return "FAILED: %s" % (e)   
+        return "FAILED: %s" % (e)
 
 
-def activate_parcel(cluster, parcel):
+def activate_parcel(cluster, parcel, timeout):
     try:
         pd = parcel.activate()
         if pd.success != True:
             result = "FAILED: Could not activate parcel"
-        while parcel.stage != 'ACTIVATED':
+        curr_timeout = 0
+        while parcel.stage != 'ACTIVATED' and curr_timeout < timeout:
             parcel = cluster.get_parcel(parcel.product, parcel.version)
             if parcel.state.errors:
                 return "FAILED: " + str(parcel.state.errors) + '.'
             time.sleep(15)
+            curr_timeout += 15
         if parcel.stage == 'ACTIVATED':
-            return "OK" 
+            return "OK"
+        return "FAILED: Timed out"
     except Exception as e:
         return "FAILED: %s" % (e)   
 
 
-def handle_parcels(api, parcels, action_func):
+def handle_parcels(api, parcels, action_func, timeout):
     results = dict()
     for cluster in api.get_all_clusters():
         results[cluster.name] = dict()
@@ -60,7 +69,7 @@ def handle_parcels(api, parcels, action_func):
             parcel = cluster.get_parcel(service, version)
             results[cluster.name][service] = dict()
             results[cluster.name][service][version] = dict()
-            results[cluster.name][service][version] = action_func(cluster, parcel)
+            results[cluster.name][service][version] = action_func(cluster, parcel, timeout)
     return results
 
 
@@ -76,7 +85,8 @@ def main():
                 user = dict(default = None),
                 password = dict(default = None),
                 parcels = dict(type = 'dict', required = True),
-                action = dict(type = 'str', choices = ['download', 'distribute', 'activate'], default = [])
+                action = dict(type = 'str', choices = ['download', 'distribute', 'activate'], default = []),
+                timeout = dict(type = 'int', default = 180)
                 ),
             )
 
@@ -86,6 +96,7 @@ def main():
     password = module.params['password']
     parcels = module.params['parcels']
     action = module.params['action']
+    timeout = module.params['timeout']
 
     api = cm_api(host, api_version, user, password)
 
@@ -95,7 +106,7 @@ def main():
         'activate': activate_parcel
     }[action]
 
-    parcels = handle_parcels(api, parcels, func)
+    parcels = handle_parcels(api, parcels, func, timeout)
 
     result = dict(
             results=dict(
